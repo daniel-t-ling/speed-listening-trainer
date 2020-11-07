29dd2751eb2cfc69d4ac665f436ee522391269c7
Jason <67932604+UserSorrow@users.noreply.github.com>
2020-11-07 16:57:11 -0500

    update

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-alert(); //test
 // HTML Element framework
 class TextElement {
     constructor(elementId) {
@@ -8,21 +7,13 @@ class TextElement {
     get element() {
         return document.getElementById(this._elementId);
     }
-
-    hide() {
-        this.element.style.display = none;
-    }
-
-    show() {
-        this.element.style.display = inline;
-    }
 }
 
 let speed = 1; // default value
 
 // Input stuffs
 const inputs = [];
-class InputE extends TextElement{
+class InputE extends TextElement {
     constructor(elementId, labelId) {
         super(elementId);
         this._labelId = labelId;
@@ -33,16 +24,12 @@ class InputE extends TextElement{
     get label() {
         return document.getElementById(this._labelId);
     }
-
-    change() {
-        speed = this.element.value;
-        this.label.innerHTML = `Playback Speed: ${speed}`;
-    }
-}
-
-function change() {
-
 }
 
 const audioSlider = new InputE("speedSlider", "speedSliderLabel");
-audioSlider.element.addEventListener("onchange", audioSlider.change());
+function setSpeed() {
+    speed = audioSlider.element.value;
+    audioSlider.label.innerHTML = `Playback Speed: ${speed}`;
+}
+
+audioSlider.element.addEventListener("input", setSpeed);
