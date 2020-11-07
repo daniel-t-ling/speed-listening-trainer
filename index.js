74ec226d3edeafc7e8972f772b6351727c659ff3
Jason <67932604+UserSorrow@users.noreply.github.com>
2020-11-07 15:18:46 -0500

    added slider

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-alert("hello!");
+// Element framework
+class HElement {
+    constructor(buttonId) {
+        this._buttonId = buttonId;
+    }
+
+    get element() {
+        document.getElementById(this._buttonId);
+    }
+
+    hide() {
+        this.element.style.display = none;
+    }
+
+    show() {
+        this.element.style.display = inline;
+    }
+}
+
+
+// Input stuffs
+const inputs = [];
+class Input extends HElement{
+    constructor(buttonId) {
+        super();
+    }
+}
+
+const audioSlider = new Input("speedSlider");
+
